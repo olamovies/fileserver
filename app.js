2ecbf3d281e9af2f62e41070ea23a54c57f882bc
olamovies <67856107+olamovies@users.noreply.github.com>
2020-07-17 14:52:52 +0600

    Update app.js
    3

--- app.js
+++ app.js
@@ -211,22 +211,7 @@ function list(path) {
   var content = `
 	<div id="head_md" class="mdui-typo" style="display:none;padding: 20px 0;"></div>
 	 <div class="mdui-row">
-	  <ul class="mdui-list">
-	   <li class="mdui-list-item th">
-	    <div class="mdui-col-xs-12 mdui-col-sm-7">
-	     File
-	<i class="mdui-icon material-icons icon-sort" data-sort="name" data-order="more">expand_more</i>
-	    </div>
-	    <div class="mdui-col-sm-3 mdui-text-right">
-	     Time
-	<i class="mdui-icon material-icons icon-sort" data-sort="date" data-order="downward">expand_more</i>
-	    </div>
-	    <div class="mdui-col-sm-2 mdui-text-right">
-	     Size
-	<i class="mdui-icon material-icons icon-sort" data-sort="size" data-order="downward">expand_more</i>
-	    </div>
-	    </li>
-	  </ul>
+	  File Server by OlaMovies.Top
 	 </div>
 	 <div class="mdui-row">
 	  <ul id="list" class="mdui-list">
